99a0bcfbd2f34c912a21b42a9dfb4d928cd794d8
IordacheCursuri <mihai.iordache82@gmail.com>
2023-11-25 23:18:31 +0200

    first commit for api

---
+++ trains-api/src/main/java/com/gini/TrainsApiApplication.java
@@ -0,0 +1,13 @@
+package com.gini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainsApiApplication.class, args);
+    }
+
+}
