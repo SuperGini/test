ab33f43e264d7bcd6af86920b8dcb12ae37ad979
IordacheCursuri <mihai.iordache82@gmail.com>
2023-11-28 23:34:09 +0200

    initializing auth-server

---
+++ trains-auth-server/src/main/java/com/gini/TrainsAuthServerApplication.java
@@ -0,0 +1,13 @@
+package com.gini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainsAuthServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainsAuthServerApplication.class, args);
+    }
+
+}
