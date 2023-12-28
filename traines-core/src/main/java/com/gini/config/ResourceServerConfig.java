96390ce7db10935fb037e7a155c66dcecc374e4b
IordacheCursuri <mihai.iordache82@gmail.com>
2023-12-28 23:36:28 +0200

    made service statless

--- traines-core/src/main/java/com/gini/config/ResourceServerConfig.java
+++ traines-core/src/main/java/com/gini/config/ResourceServerConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
 import org.springframework.security.oauth2.core.OAuth2TokenValidator;
 import org.springframework.security.oauth2.jwt.Jwt;
@@ -29,6 +30,8 @@ public class ResourceServerConfig {
                         .requestMatchers("/route/**").authenticated()
                         .anyRequest().authenticated()
         );
+
+        http.sessionManagement(x -> x.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         http.oauth2ResourceServer(oauth2 ->
                 oauth2.jwt(jwt -> jwt.decoder(decoder))
